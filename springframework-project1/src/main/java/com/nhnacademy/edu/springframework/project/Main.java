07399dab216676fc806fbdff0234371242e669aa
halo-eunsu <ujd017@naver.com>
2024-02-05 05:42:13 +0900

    1

--- springframework-project1/src/main/java/com/nhnacademy/edu/springframework/project/Main.java
+++ springframework-project1/src/main/java/com/nhnacademy/edu/springframework/project/Main.java
@@ -1,9 +1,13 @@
 package com.nhnacademy.edu.springframework.project;
 
+import com.nhnacademy.edu.springframework.project.config.AppConfig;
+import com.nhnacademy.edu.springframework.project.repository.StudentService;
 import com.nhnacademy.edu.springframework.project.service.CsvDataLoadService;
 import com.nhnacademy.edu.springframework.project.service.DataLoadService;
 import com.nhnacademy.edu.springframework.project.service.DefaultStudentService;
 import com.nhnacademy.edu.springframework.project.service.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Collection;
 
@@ -11,14 +15,25 @@ public class Main {
 
     // TODO 9 - 성공적으로 실행되어야 합니다.
     public static void main(String[] args) {
-        DataLoadService dataLoadService = new CsvDataLoadService();
-        dataLoadService.loadAndMerge();
 
-        DefaultStudentService studentService = new DefaultStudentService();
-        Collection<Student> passedStudents = studentService.getPassedStudents();
-        System.out.println(passedStudents);
 
-        Collection<Student> orderedStudents = studentService.getStudentsOrderByScore();
-        System.out.println(orderedStudents);
+        try(AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class)){
+
+            DataLoadService dataLoadService = context.getBean(DataLoadService.class);
+            dataLoadService.loadAndMerge();
+
+            StudentService studentService = context.getBean(StudentService.class);
+            Collection<Student> passedStudents = studentService.getPassedStudents();
+            System.out.println(passedStudents);
+
+            Collection<Student> orderedStudents = studentService.getStudentsOrderByScore();
+            System.out.println(orderedStudents);
+        }
+
+
+
+
+
+
     }
 }
--- springframework-project1/src/main/java/com/nhnacademy/edu/springframework/project/service/DefaultGradeQueryService.java
+++ springframework-project1/src/main/java/com/nhnacademy/edu/springframework/project/service/DefaultGradeQueryService.java
@@ -1,11 +1,13 @@
 package com.nhnacademy.edu.springframework.project.service;
 
 import com.nhnacademy.edu.springframework.project.repository.*;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Component
 public class DefaultGradeQueryService implements GradeQueryService {
 
     @Override
