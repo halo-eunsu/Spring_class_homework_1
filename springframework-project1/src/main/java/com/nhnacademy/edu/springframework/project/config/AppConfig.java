b2b91bbc83bbeafe7e26a11d5079c6fdd728887f
halo-eunsu <ujd017@naver.com>
2024-02-05 05:24:10 +0900

    1

--- springframework-project1/src/main/java/com/nhnacademy/edu/springframework/project/config/AppConfig.java
+++ springframework-project1/src/main/java/com/nhnacademy/edu/springframework/project/config/AppConfig.java
@@ -1,4 +1,11 @@
 package com.nhnacademy.edu.springframework.project.config;
 
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.nhnacademy.edu.springframework.project")
 public class AppConfig {
 }
